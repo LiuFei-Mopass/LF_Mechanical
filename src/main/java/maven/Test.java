1c88ddf79f59c8c9d739e955fc634810dc25f4d2
LiuFei <liufei_work@sina.com>
2017-03-03 14:35:10 +0800

    添加JDBC连接

--- src/main/java/maven/Test.java
+++ src/main/java/maven/Test.java
@@ -1,13 +1,13 @@
 package maven;
 
+import org.apache.log4j.Logger;
+
 public class Test {
 	
-	public static void main(String[] args) {
-		
-		
-		
-		String t  = "123456789";
-		System.out.println(t.substring(0,5));
-	}
+	public static void main(String[] args) throws Exception {
+        Logger logger = Logger.getLogger("Mechanical");
+        logger.info("开始");
+        logger.debug("结束");
+    }
 
 }
