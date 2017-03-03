1751a700107c178f72b46cc0ea0d891b1504c1d1
LiuFei <liufei_work@sina.com>
2017-03-03 13:34:34 +0800

    添加部分文件

---
+++ src/main/java/com/lf/controlle/reception/springmvcDemo.java
@@ -0,0 +1,17 @@
+package com.lf.controlle.reception;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+public class springmvcDemo {
+	
+	
+	@RequestMapping("/index")
+    public String index(){
+        return "springmvc-demo";
+    }
+
+}
---
+++ src/main/java/maven/Test.java
@@ -0,0 +1,13 @@
+package maven;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		String t  = "123456789";
+		System.out.println(t.substring(0,5));
+	}
+
+}
