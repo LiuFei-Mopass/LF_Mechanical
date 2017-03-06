470ab524829cc7f08db987c32e6cdb121299ce8a
LiuFei <liufei_work@sina.com>
2017-03-06 09:25:10 +0800

    修改冲突

--- src/main/java/maven/Test.java
+++ src/main/java/maven/Test.java
@@ -1,26 +1,16 @@
 package maven;
 
-import java.util.HashMap;
-import java.util.Map;
 
-import com.lf.util.jdbc.JDBCUtils;
+
+import org.apache.log4j.Logger;
 
 public class Test {
 	
+	
 	public static void main(String[] args) throws Exception {
-       
-		
-		Map<String , String> map = new HashMap<String , String>();
-		
-		
-		map.put("user_name", "lf");
-		map.put("user_password", "lf");
-		
-		String where = "user_id='3'";
-		
-		int insert = JDBCUtils.update_s("bac_user", map, where);
-		System.out.println("====>"+insert);
-		
+        Logger logger = Logger.getLogger("Mechanical");
+        logger.info("开始");
+        logger.debug("结束");
     }
 
 }
