0893e5c1c87ee3691216b2d4c745b548a5b27b54
LiuFei <liufei_work@sina.com>
2017-03-06 16:58:35 +0800

    搭建架构

--- src/main/java/com/lf/reception/controlle/springmvcDemo.java
+++ src/main/java/com/lf/reception/controlle/springmvcDemo.java
@@ -1,4 +1,4 @@
-package com.lf.controlle.reception;
+package com.lf.reception.controlle;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
